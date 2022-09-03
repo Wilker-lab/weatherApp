6620ff08780f363ef6f6bfa910a58413e83a3113
wilker.macedo <wilkermaceedo@gmail.com>
2022-09-03 10:17:15 -0300

    first commit

---
+++ app/src/main/java/com/wilker/weatherapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wilker.weatherapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wilker.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
